f51483bc33819442e2d8b3ac9e2fdd62152d7329
LABCAT <shane.watters.nz@gmail.com>
2021-01-22 09:07:07 +1300

    Removing code that shouldn't have been commited from functions.php.

--- public/app/themes/mytheme/functions.php
+++ public/app/themes/mytheme/functions.php
@@ -2,7 +2,3 @@
 
 add_theme_support( 'post-thumbnails' );
 add_theme_support( 'align-wide' );
-
-@ini_set( 'upload_max_size' , '64M' );
-@ini_set( 'post_max_size', '64M');
-@ini_set( 'max_execution_time', '300' );
